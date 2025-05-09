92ff003176f0d68f53426c7fb525ae9a98badc21
Orkhan Alizada <70582593+orkhanalizada2020@users.noreply.github.com>
2025-05-09 14:19:02 +0200

    SPM support added.

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ATOMConsentSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ATOMConsentSDK",
+            targets: ["ATOMConsentSDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ATOMTCFConsent"),
+        .target(
+            name: "ATOMCCPAConsent"),
+        .testTarget(
+            name: "ATOMConsentSDKTests",
+            dependencies: ["ATOMConsentSDK"]
+        ),
+    ]
+)
